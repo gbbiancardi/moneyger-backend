32d55a5ce36fb3400f6fa9cc189514d11278dc05
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2020-01-17 14:06:05 -0300

    User update.

--- src/main/java/br/com/gerenciador/moneyger/resources/UserResource.java
+++ src/main/java/br/com/gerenciador/moneyger/resources/UserResource.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.com.gerenciador.moneyger.model.User;
 import br.com.gerenciador.moneyger.services.UserService;
@@ -47,5 +50,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
